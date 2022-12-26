62d81028bba8c5addadfc49b50dee5612416546d
ender <ender.1895.perez@gmail.com>
2022-12-26 16:14:23 -0600

    clean inputs on create

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-let nameTag = document.querySelector("#name-tag");
-let ParrafoTag = document.querySelector("#parrafo-tag");
-let imgPresent= document.getElementById('img_presen');
+let slideCounter=1;
+
+var nameTag = document.querySelector("#name-tag");
+var ParrafoTag = document.querySelector("#parrafo-tag");
+var imgPresent= document.getElementById('img_presen');
 
 
-function setName(element) {
-    console.log(element.value);
-    nameTag.innerText = element.value;
-}
-
-
-function setparrafo(element) {
-    console.log(element.value);
-    ParrafoTag.innerText = element.value;
-}
-
-
+/*
 function carro1(){
     console.log('presiono carro 1')
     imgPresent.src= 'img/carro1.png';
@@ -23,10 +14,34 @@ function carro1(){
 function carro2(){
     imgPresent.src= 'img/carro2.png';
 }
+
 function todo(){
     console.log('cualquier imagen')
 }
+*/
 
-function crear() {
+function setName(element) {
+        //console.log(element.value);
+        nameTag.innerText = element.value;
+    }
+     
+    
+function setparrafo(element) {
+        //console.log(element.value);
+        ParrafoTag.innerHTML = element.value;
+}
+
+function createSlide() {
+    // Reset Inputs
+    document.getElementById('slide-title-input').value = '';
+    document.getElementById('slide-content-input').value = '';
+
+
+    
+    console.log(slideCounter)
     console.log('crear hoja');
-    document.getElementById("container").innerHTML = '<div class="cuadro_blanco"> <h1 id="name-tag">TITULO</h1><div class="imagen"><p id="parrafo-tag">Parrafo</p><img id="img_presen" src="https://www.befunky.com/images/prismic/d98b04bd-7bb6-4e31-832f-5800572ce488_how-to_blur-image-21.svg" alt="imagen"></div></div>'}
+    document.getElementById("slide-preview").innerHTML += '<div class="cuadro_blanco" id="'+slideCounter+'"> <h1 id="name-tag">TITULO</h1><div class="imagen"><p id="parrafo-tag">Parrafo</p><img id="img_presen" src="https://www.befunky.com/images/prismic/d98b04bd-7bb6-4e31-832f-5800572ce488_how-to_blur-image-21.svg" alt="imagen"></div></div>';
+    slideCounter++; 
+    setName();
+    setparrafo();
+}
