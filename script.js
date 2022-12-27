1792df38a557ec0557af243feabb68e351afda71
ender <ender.1895.perez@gmail.com>
2022-12-27 15:22:33 -0600

    todo ok falta seleccion de recuadro para editar

--- script.js
+++ script.js
@@ -37,12 +37,20 @@ function guardar(){
     
 }
 
+const clickOnSlideHandler = (event) => {
+    console.log('event', event);
+    console.log('event.target', event.target);
+    event.target.classList.add('selected-slide');
+}
+
+
 function createSlide() {
     //Duplicate Element and add to slides containers
     const slideDraft = document.getElementById('slide-draft');
     const newSlide = slideDraft.cloneNode(true);
     newSlide.id =  `slide-${slideCounter}`;
     newSlide.classList.add('slide');
+    newSlide.addEventListener('click', clickOnSlideHandler)
     
 
     //nueva cuadro blanco sea original anted de editar
@@ -55,5 +63,11 @@ function createSlide() {
     //document.getElementById("slides").innerHTML += '<div class="cuadro_blanco" id="'+slideCounter+'"> <h1 id="name-tag">TITULO</h1><div class="imagen"><p id="parrafo-tag">Parrafo</p><img id="img_presen" src="https://www.befunky.com/images/prismic/d98b04bd-7bb6-4e31-832f-5800572ce488_how-to_blur-image-21.svg" alt="imagen"></div></div>';
     document.getElementById("slides").append(newSlide);
     slideCounter++;
+
+    
+    //const slides = document.getElementsByClassName("slide");
+    //console.log('slides:', slides);
 }
 
+
+const slides = document.getElementsByClassName("slide");
