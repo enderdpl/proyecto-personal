43fb7a7613359a7eab0b5569d9cb73e50bdcd931
ender <ender.1895.perez@gmail.com>
2022-12-26 18:34:37 -0600

    add id and class for new slide

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var ParrafoTag = document.querySelector("#parrafo-tag");
 var imgPresent= document.getElementById('img_presen');
 
 
-/*
+
 function carro1(){
     console.log('presiono carro 1')
     imgPresent.src= 'img/carro1.png';
@@ -18,7 +18,7 @@ function carro2(){
 function todo(){
     console.log('cualquier imagen')
 }
-*/
+
 
 function setName(element) {
         //console.log(element.value);
@@ -30,18 +30,30 @@ function setparrafo(element) {
         //console.log(element.value);
         ParrafoTag.innerHTML = element.value;
 }
-
-function createSlide() {
+function guardar(){
     // Reset Inputs
     document.getElementById('slide-title-input').value = '';
     document.getElementById('slide-content-input').value = '';
-
-
     
-    console.log(slideCounter)
-    console.log('crear hoja');
-    document.getElementById("slide-preview").innerHTML += '<div class="cuadro_blanco" id="'+slideCounter+'"> <h1 id="name-tag">TITULO</h1><div class="imagen"><p id="parrafo-tag">Parrafo</p><img id="img_presen" src="https://www.befunky.com/images/prismic/d98b04bd-7bb6-4e31-832f-5800572ce488_how-to_blur-image-21.svg" alt="imagen"></div></div>';
-    slideCounter++; 
-    setName();
-    setparrafo();
 }
+
+function createSlide() {
+    //Duplicate Element and add to slides containers
+    const slideDraft = document.getElementById('slide-draft');
+    const newSlide = slideDraft.cloneNode(true);
+    newSlide.id =  `slide-${slideCounter}`;
+    newSlide.classList.add('slide');
+    
+
+    //nueva cuadro blanco sea original anted de editar
+    nameTag.innerHTML= "TITULO";
+    ParrafoTag.innerHTML= "Parrafo";
+    imgPresent.src='img/foto.png';
+
+    console.log('slideCounter', slideCounter);
+
+    //document.getElementById("slides").innerHTML += '<div class="cuadro_blanco" id="'+slideCounter+'"> <h1 id="name-tag">TITULO</h1><div class="imagen"><p id="parrafo-tag">Parrafo</p><img id="img_presen" src="https://www.befunky.com/images/prismic/d98b04bd-7bb6-4e31-832f-5800572ce488_how-to_blur-image-21.svg" alt="imagen"></div></div>';
+    document.getElementById("slides").append(newSlide);
+    slideCounter++;
+}
+
